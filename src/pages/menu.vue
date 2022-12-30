ec81241658f3811c5e2e9f39c7418c023aa233a7
BADER <badr.chraibibadr@gmail.com>
2022-12-30 23:29:49 +0100

    adding global menu

---
+++ src/pages/menu.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header bordered>
+      <q-toolbar>
+        <q-toolbar-title> Foot Fans Only </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container class="window-height">
+      <div class="q-pa-md full-height">
+        <q-card bordered>
+          <q-card-section>
+            <div class="row q-col-gutter-x-sm q-col-gutter-y-sm justify-center">
+              <div class="col-xs-12 col-md-6">
+                <q-btn class="fit" color="primary" stack to="event">
+                  <q-icon size="8em" name="person" />
+                  <div>Joueur</div>
+                </q-btn>
+              </div>
+              <div class="col-xs-12 col-md-6">
+                <q-btn class="fit" color="primary" stack to="pitch">
+                  <q-icon size="8em" name="stadium" />
+                  <div>Proprietaire</div>
+                </q-btn>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'AppsMenu',
+
+  setup() {
+
+    return {
+      link: ref(null),
+    };
+  },
+};
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,7 +3,11 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('pages/menu.vue'),
+  },
+  {
+    path: '/event',
+    component: () => import('layouts/EventLayout.vue'),
     children: [
       {
         meta: {
@@ -26,24 +30,29 @@ const routes: RouteRecordRaw[] = [
         path: '/event/demands',
         component: () => import('pages/event/eventDemands.vue'),
       },
+    ],
+  },
 
+  {
+    path: '',
+    component: () => import('layouts/PitchLayout.vue'),
+    children: [
       {
         meta: {
           title: 'Nouveau terrain',
         },
-        path: '/terrain/new',
+        path: '/pitch/new',
         component: () => import('pages/terrain/newTerrain.vue'),
       },
       {
         meta: {
           title: 'Les terrains',
         },
-        path: '/terrain',
+        path: '/pitch',
         component: () => import('pages/terrain/terrains.vue'),
       },
     ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
