c762232e1f1d1e6d08875df22c967553fb1d3261
BADER <badr.chraibibadr@gmail.com>
2023-01-06 22:47:45 +0100

    delete terrain with backend

--- src/pages/terrain/terrains.vue
+++ src/pages/terrain/terrains.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page>
-    <q-table ref="eventTableRef" title="Vos terrains" no-data-label="Aucun enregistrements trouvés"
-      no-results-label="Aucun enregistrements correspondants trouvés" loading-label="Chargement"
+    <q-table title="Vos terrains" no-data-label="Aucun terrain trouvé"
+      no-results-label="Aucun terrain correspondant trouvés" loading-label="Chargement"
       rows-per-page-label="Element par page" :dense="$q.screen.lt.md" :rows="rows" bordered
       table-header-class="text-primary" :columns="columns" row-key="id" :filter="filter" binary-state-sort>
       <template v-slot:top-right="props">
@@ -15,12 +15,32 @@
       </template>
       <template v-slot:body-cell-actions="props">
         <q-td :props="props">
-          <q-btn dense round flat color="green" @click="showEditDialog(props.row)" icon="edit"></q-btn>
           <q-btn dense round flat color="red" @click="showDeleteDialog(props.row)" icon="delete"></q-btn>
         </q-td>
       </template>
     </q-table>
   </q-page>
+
+  <!-- start of delete confirmation dialog -->
+  <q-dialog v-model="deleteDialog" persistent>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Suppression</div>
+      </q-card-section>
+      <q-separator inset />
+      <q-card-section class="row items-center">
+        <span class="q-mx-sm">
+          Voulez-vous vraiment supprimer cette annnoce !
+        </span>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn @click="deleteTerrain" flat label="Confirmer" color="red" v-close-popup />
+        <q-btn flat label="Annuler" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+  <!-- end of delete confirmation dialog -->
 </template>
 
 <script>
@@ -58,6 +78,12 @@ const columns = [
     align: 'center',
     sortable: true,
   },
+  {
+    name: 'actions',
+    required: true,
+    label: 'Actions',
+    align: 'center',
+  },
 ];
 
 async function loadTerrains() {
@@ -75,9 +101,29 @@ export default ({
       rows.value = await loadTerrains();
     });
 
+    // delete event function
+    const deleteTerrain = async () => {
+      await axios.delete('terrains/delete/' + selectedTerrain.value);
+      rows.value = await loadTerrains();
+    };
+
+    const deleteDialog = ref(false);
+    const selectedTerrain = ref(null);
+
+
+    // show delete dialog function
+    function showDeleteDialog(item) {
+      selectedTerrain.value = item.id;
+      deleteDialog.value = true;
+    }
+
     return {
       columns,
       rows,
+      deleteDialog,
+      showDeleteDialog,
+      deleteTerrain,
+      selectedTerrain,
       filter: ref(''),
     }
   }
